9fa23fc678231841ed02ddd2f207454ec2477c4d
Jakob Marco <marco.jakob1@mobi.ch>
2021-06-04 16:33:29 +0200

    block chain POW Demo

--- src/blocks/Block.tsx
+++ src/blocks/Block.tsx
@@ -2,7 +2,8 @@ import {
   Button,
   Card,
   CardActions,
-  CardContent, CircularProgress,
+  CardContent,
+  Grid,
   List,
   ListItemText,
   TextField,
@@ -10,8 +11,8 @@ import {
 } from "@material-ui/core";
 import React, {ChangeEvent, useEffect, useState} from "react";
 import {sha256} from "../utils/cryptoUtils";
-import {BehaviorSubject, EMPTY, Observable, of, Subject, Subscription} from "rxjs";
-import {expand, switchMap} from "rxjs/operators";
+import {BehaviorSubject, merge, Observable, of} from "rxjs";
+import {delay, expand, map, switchMap, takeWhile} from "rxjs/operators";
 import {ChainBlock} from "./BlockList";
 import {cardStyle} from "../theming/theme";
 import {useTranslation} from "react-i18next";
@@ -22,36 +23,116 @@ type BlockInputProps = {
   previousHash: string
   availableBlocks: ChainBlock[]
   hashChanged: (blockId: number, hash: string) => void
+  miners: Miner[]
 }
 
-function initialHashState(initialInput: string) : [Observable<string>, (val: string) => void] {
+type MinerDisplay = {
+  name: string
+  nonce: number
+  hash: string
+}
+
+export class Miner {
+  private _nonce: number;
+  private _hash: string;
+
+  constructor(private _delay: number, private _name: string) {
+    this._nonce = 0;
+    this._hash = '';
+  }
+
+  get delay(): number {
+    return this._delay;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get nonce(): number {
+    return this._nonce;
+  }
+
+  get hash(): string {
+    return this._hash;
+  }
+
+
+  set nonce(value: number) {
+    this._nonce = value;
+  }
+
+  set hash(value: string) {
+    this._hash = value;
+  }
+
+  toDisplay(): MinerDisplay {
+    return {name: this._name, nonce: this._nonce, hash: this._hash}
+  }
+}
+
+const minerCard = {
+  ...cardStyle,
+  width: undefined,
+  backgroundColor: '#FFFFFF',
+  opacity: '100%',
+  margin: '5px',
+  padding: '10px'
+
+}
+
+function initialHashState(initialInput: string): [Observable<string>, (val: string) => void] {
   const subject = new BehaviorSubject(initialInput);
-  const putVal:  (val: string) => void = (newVal: string) => {
+  const putVal: (val: string) => void = (newVal: string) => {
     subject.next(newVal);
   }
   const start = subject.pipe(switchMap((val: string) => sha256(val)))
   return [start, putVal]
 }
 
+function initMiner(miner: Miner, content: string): Observable<MinerDisplay> {
+  return of({
+    name: miner.name,
+    hash: '',
+    nonce: 0
+  }).pipe(expand((currentMiner: MinerDisplay) => {
+    miner.hash = currentMiner.hash
+    miner.nonce = currentMiner.nonce
+    const newNonce = Math.floor(Math.random() * 500)
+    return sha256(content + (newNonce)).pipe(delay(miner.delay), map((hash: string) => ({
+      name: miner.name,
+      hash,
+      nonce: newNonce
+    })));
+  }));
+}
+
 function Block(props: BlockInputProps) {
   const {t} = useTranslation();
   const [[calHashObservable$, putContentVal], _] = useState(initialHashState(''));
-  const [callbackFn, setCallbackFn] = useState(() => (val: string) => {});
   const [calHash, setCalHash] = useState('')
-  const [isLoading, setLoading] = useState<boolean>(false)
   const [nonce, setNonce] = useState(0);
   const [textContent, setTextContent] = useState('')
+  const [displayMiners, setMiners] = useState<MinerDisplay[]>([])
 
   function updateHash(hash: string) {
     setCalHash(hash);
   }
 
   useEffect(() => {
+    setMiners(props.miners.map((miner: Miner) => ({
+      name: miner.name,
+      nonce: miner.nonce,
+      hash: miner.hash
+    })))
+  }, [props.miners])
+
+  useEffect(() => {
     props.hashChanged(props.blockId, calHash);
   }, [calHash])
 
   useEffect(() => {
-    putContentVal(textContent+ props.previousHash + nonce)
+    putContentVal(textContent + props.previousHash + nonce)
   }, [props.previousHash])
 
 
@@ -65,40 +146,70 @@ function Block(props: BlockInputProps) {
   function textFieldChange(changeElement: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
     const text = changeElement.target.value;
     setTextContent(text);
-    putContentVal(text+ props.previousHash + nonce)
+    putContentVal(text + props.previousHash + nonce)
   }
 
   async function calculateHash(): Promise<void> {
-    let currentNonce = 0;
-    of('').pipe(expand((currentHash: string) => {
-      if(currentHash.startsWith('0')){
-        updateHash(currentHash)
-        return EMPTY;
+    const miners: Observable<MinerDisplay>[] = props.miners.map((miner: Miner) => initMiner(miner, props.previousHash + textContent));
+
+    merge(...miners).pipe(map((miner: MinerDisplay) => {
+      const updatedMiners: MinerDisplay[] = displayMiners.map((minerTmp: MinerDisplay) => {
+        if (minerTmp.name === miner.name) {
+          return miner
+        }
+        return minerTmp
+
+      })
+      setMiners(updatedMiners)
+      if (miner.hash.startsWith('0')) {
+        updateHash(miner.hash)
+        setNonce(miner.nonce)
+        return true;
       }
-      currentNonce = currentNonce +1;
-      setNonce(() => currentNonce)
-      updateHash(currentHash)
-      return sha256(textContent+props.previousHash+(currentNonce));
-    })).subscribe();
+      return false
+    }), takeWhile((result: boolean) => !result)).subscribe()
 
   }
 
   return (
-    <Card style={{...cardStyle, width: '500px', backgroundColor: calHash.startsWith('0')? '#cde7c0': '#ff000038', margin: '10px'}}>
-      <CardContent>
-        <Typography variant="h6">
-          {t('demo.heading') +props.blockId}
-        </Typography>
-        <TextField onChange={textFieldChange} multiline label={t('demo.content')} rows={5} variant={'outlined'}/>
-        <List>
-          <ListItemText primary={t('demo.predecessor')} secondary={props.previousHash} />
-          <ListItemText primary={t('demo.nonce')} secondary={nonce} />
-          <ListItemText primary={t('demo.hash')} secondary={calHash} />
-        </List>
-        {isLoading && <CircularProgress />}
-      </CardContent>
-      <CardActions><Button onClick={calculateHash}>{t('demo.mine')}</Button></CardActions>
-    </Card>
+      <Card style={{
+        ...cardStyle,
+        width: '500px',
+        backgroundColor: calHash.startsWith('0') ? '#cde7c0' : '#ff000038',
+        margin: '10px',
+        opacity: '100%',
+      }}>
+        <CardContent>
+          <Typography variant="h6">
+            {t('demo.heading') + props.blockId}
+          </Typography>
+          <TextField onChange={textFieldChange} multiline label={t('demo.content')} rows={5}
+                     variant={'outlined'}/>
+          <List>
+            <ListItemText primary={t('demo.predecessor')} secondary={props.previousHash}/>
+            <ListItemText primary={t('demo.nonce')} secondary={nonce}/>
+            <ListItemText primary={t('demo.hash')} secondary={calHash}/>
+            <Grid container direction={'row'} justify={'space-around'} alignItems={'center'}>
+              {displayMiners.map((miner: MinerDisplay) => (
+                  <Grid key={miner.name} item xs={6} style={{justifyContent: 'flex-start'}}>
+                    <Card style={{
+                      ...minerCard,
+                      backgroundColor: miner.hash === calHash ? cardStyle.backgroundColor : minerCard.backgroundColor
+                    }}>
+                      <List>
+                        <ListItemText primary={'Name'} secondary={miner.name}/>
+                        <ListItemText primary={'nonce'} secondary={miner.nonce}/>
+                        <ListItemText style={{textOverflow: 'ellipsis', overflow: 'hidden'}}
+                                      primary={'hash'} secondary={miner.hash}/>
+                      </List>
+                    </Card>
+                  </Grid>
+              ))}
+            </Grid>
+          </List>
+        </CardContent>
+        <CardActions><Button onClick={calculateHash}>{t('demo.mine')}</Button></CardActions>
+      </Card>
   )
 }
 
--- src/blocks/BlockList.tsx
+++ src/blocks/BlockList.tsx
@@ -1,126 +1,196 @@
-import {Card, CardContent, Grid, IconButton, Typography} from "@material-ui/core";
-import Block from "./Block";
+import {
+  Card,
+  CardContent,
+  Grid,
+  IconButton,
+  Slider,
+  TextField,
+  Typography
+} from "@material-ui/core";
+import Block, {Miner} from "./Block";
 import AddIcon from "@material-ui/icons/Add";
-import React, {useEffect, useState} from "react";
+import React, {ChangeEvent, useEffect, useState} from "react";
 import {cardStyle} from "../theming/theme";
 import {useTranslation} from "react-i18next";
 
 export interface ChainBlock {
-    id: number;
-    hash?: string;
-    previousHash: string;
+  id: number;
+  hash?: string;
+  previousHash: string;
 }
 
 interface AddAction {
-    type: 'add'
-    payload: ChainBlock
+  type: 'add'
+  payload: ChainBlock
 }
 
 interface DeleteAction {
-    type: 'remove'
-    payload: number
+  type: 'remove'
+  payload: number
 }
 
 interface UpdateAction {
-    type: 'update'
-    payload: number
-    hash: string
+  type: 'update'
+  payload: number
+  hash: string
 }
 
-function blocksReducer(state: ChainBlock[], action: AddAction | DeleteAction | UpdateAction): ChainBlock[] {
-    switch (action.type) {
-        case "add":
-            return [...state, action.payload]
-        case "remove":
-            return state.filter((chainBlock: ChainBlock) => action.payload !== chainBlock.id)
-        case "update": {
-            const changedBlock = state.find((block: ChainBlock) => block.id === action.payload);
-            if (changedBlock) {
-                const nextBlock = state.find((block: ChainBlock) => block.id === (changedBlock.id +1));
-                const unchangedBlocks = state.filter((block: ChainBlock) => {
-                    if(!!nextBlock){
-                        return block.id !== action.payload &&  block.id !== nextBlock.id
-                    }
-                    return block.id !== action.payload
-                });
-                if(!!nextBlock){
-                    return [...unchangedBlocks, ...[{...changedBlock, hash: action.hash}, {...nextBlock, previousHash: action.hash}]]
-                }
-                return [...unchangedBlocks, {...changedBlock, hash: action.hash}]
-            }
-            return state
-        }
-        default:
-            return state
 
+function blocksReducer(state: ChainBlock[], action: AddAction | DeleteAction | UpdateAction): ChainBlock[] {
+  switch (action.type) {
+    case "add":
+      return [...state, action.payload]
+    case "remove":
+      return state.filter((chainBlock: ChainBlock) => action.payload !== chainBlock.id)
+    case "update": {
+      const changedBlock = state.find((block: ChainBlock) => block.id === action.payload);
+      if (changedBlock) {
+        const nextBlock = state.find((block: ChainBlock) => block.id === (changedBlock.id + 1));
+        const unchangedBlocks = state.filter((block: ChainBlock) => {
+          if (!!nextBlock) {
+            return block.id !== action.payload && block.id !== nextBlock.id
+          }
+          return block.id !== action.payload
+        });
+        if (!!nextBlock) {
+          return [...unchangedBlocks, ...[{...changedBlock, hash: action.hash}, {
+            ...nextBlock,
+            previousHash: action.hash
+          }]]
+        }
+        return [...unchangedBlocks, {...changedBlock, hash: action.hash}]
+      }
+      return state
     }
+    default:
+      return state
+
+  }
 }
 
 function useReducer(reducer: (state: ChainBlock[], action: AddAction | DeleteAction | UpdateAction) => ChainBlock[], initialState: ChainBlock[]): [ChainBlock[], (action: AddAction | DeleteAction | UpdateAction) => void] {
-    const [state, setState] = useState(initialState);
+  const [state, setState] = useState(initialState);
 
-    function dispatch(action: AddAction | DeleteAction | UpdateAction) {
-        const nextState = reducer(state, action);
-        setState(nextState);
-    }
+  function dispatch(action: AddAction | DeleteAction | UpdateAction) {
+    const nextState = reducer(state, action);
+    setState(nextState);
+  }
 
-    return [state, dispatch];
+  return [state, dispatch];
 }
 
 
 function BlockList() {
 
-    const {t} = useTranslation();
-    const [blocks, dispatch] = useReducer(blocksReducer, []);
+  const {t} = useTranslation();
+  const [blocks, dispatch] = useReducer(blocksReducer, []);
+  const [miners, setMiners] = useState([new Miner(100, 'A'), new Miner(20, 'B')])
 
-    useEffect(() => {
-        const firstBlock: ChainBlock = {id: 0, previousHash: '0000'}
-        dispatch({type: 'add', payload: firstBlock});
-    }, [])
+  useEffect(() => {
+    const firstBlock: ChainBlock = {id: 0, previousHash: '0000'}
+    dispatch({type: 'add', payload: firstBlock});
+  }, [])
+
+  function updateMiner(refIndex: number, field: 'delay' | 'name', value: string | number | number[]): void {
+    const updatedMiners = miners.map((miner: Miner, index: number) => {
+      if (index === refIndex) {
+        return field === 'delay' ? new Miner(1 / (value as number), miner.name) : new Miner(miner.delay, value as string)
+      }
+      return miner;
+    })
+    setMiners(updatedMiners)
+  }
 
 
-    const blockChanged = (blockId: number, hash: string) => {
-        dispatch({type: 'update', payload: blockId, hash})
+  const blockChanged = (blockId: number, hash: string) => {
+    dispatch({type: 'update', payload: blockId, hash})
+  }
+
+  function addBlock(): void {
+    const lastBlock = readLastBlock();
+    if (lastBlock && lastBlock.hash) {
+      dispatch({type: 'add', payload: {id: lastBlock.id + 1, previousHash: lastBlock.hash}});
     }
+  }
 
-    function addBlock(): void {
-        const lastBlock = readLastBlock();
-        if (lastBlock && lastBlock.hash) {
-            dispatch({type: 'add', payload: {id: lastBlock.id + 1, previousHash: lastBlock.hash}});
+  function readLastBlock(): ChainBlock | undefined {
+    const lastId: number = Math.max(...blocks.map((block: ChainBlock) => block.id));
+    return blocks.find((block: ChainBlock) => block.id === lastId);
+  }
+
+
+  return (
+      <div className={'blockList'}>
+        <Grid container spacing={0}
+        >
+          <Card style={cardStyle}>
+            <CardContent>
+              <TextField label={'Miner Name'}
+                         onChange={(val: any) => updateMiner(0, 'name', val.target.value)}
+
+              />
+              <Typography id="discrete-slider" gutterBottom>
+                Hash Rate
+              </Typography>
+              <Slider
+                  defaultValue={30}
+                  aria-labelledby="discrete-slider"
+                  valueLabelDisplay="auto"
+                  step={10}
+                  marks
+                  min={10}
+                  max={100}
+                  onChange={(val: ChangeEvent<{}>, newVal: number | number[]) => updateMiner(0, 'delay', newVal)}
+              />
+            </CardContent>
+          </Card>
+          <Card style={cardStyle}>
+            <CardContent>
+              <TextField label={'Miner Name'}
+                         onChange={(val: any) => updateMiner(1, 'name', val.target.value)}
+              />
+              <Typography id="discrete-slider" gutterBottom>
+                Hash Rate
+              </Typography>
+              <Slider
+                  defaultValue={100}
+                  aria-labelledby="discrete-slider"
+                  valueLabelDisplay="auto"
+                  step={10}
+                  marks
+                  min={10}
+                  max={100}
+                  onChange={(val: ChangeEvent<{}>, newVal: number | number[]) => updateMiner(1, 'delay', newVal)}
+              />
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+        >{blocks?.sort((a: ChainBlock, b: ChainBlock) => a.id - b.id).map((block: ChainBlock) => (
+            <Block key={block.id}
+                   hashChanged={(blockId: number, hash: string) => blockChanged(blockId, hash)}
+                   miners={miners}
+                   availableBlocks={blocks}
+                   blockId={block.id} previousHash={block.previousHash}/>)
+        )
         }
-    }
-
-    function readLastBlock(): ChainBlock | undefined {
-        const lastId: number = Math.max(...blocks.map((block: ChainBlock) => block.id));
-        return blocks.find((block: ChainBlock) => block.id === lastId);
-    }
-
-
-    return (
-        <div className={'blockList'}>
-            <Grid
-                container
-                direction="row"
-                justify="center"
-                alignItems="center"
-            >{blocks?.sort((a: ChainBlock, b: ChainBlock) => a.id - b.id).map((block: ChainBlock) => (
-                <Block key={block.id} hashChanged={(blockId: number, hash: string) => blockChanged(blockId, hash)}
-                       availableBlocks={blocks}
-                       blockId={block.id} previousHash={block.previousHash}/>)
-            )
-            }
-                {readLastBlock()?.hash &&
-                <Card style={cardStyle}>
-                    <CardContent>
-                        <Typography variant="h6">
-                            {t('demo.addBlock')}
-                        </Typography>
-                        <IconButton onClick={addBlock}><AddIcon color={"secondary"} fontSize={'large'}/></IconButton>
-                    </CardContent>
-                </Card>}
-            </Grid>
-        </div>
-    )
+          {readLastBlock()?.hash &&
+          <Card style={cardStyle}>
+            <CardContent>
+              <Typography variant="h6">
+                {t('demo.addBlock')}
+              </Typography>
+              <IconButton onClick={addBlock}><AddIcon color={"secondary"}
+                                                      fontSize={'large'}/></IconButton>
+            </CardContent>
+          </Card>}
+        </Grid>
+      </div>
+  )
 }
 
 export default BlockList;
